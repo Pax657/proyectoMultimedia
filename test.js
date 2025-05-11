e70b28b67ff56ba6dd8696b9a9d48d456890dd51
eldeiibid <eldeiibid@gmail.com>
2025-05-11 02:07:19 +0200

    añadido donaciones

--- test.js
+++ test.js
@@ -19,6 +19,7 @@ function toggle(id) {
     });
 }
 
+
 //Función para mostrar u ocultar bloques de forma apilada.
 //USADO PARA LA BARRA DE RECOMPENSAS
 const menus = ['menu1', 'menu2', 'menu3', 'menu4', 'menu5', 'menu6', 'menu7', 'menu8', 'menu9', 'menu10', 'menu11'];
@@ -52,3 +53,31 @@ function about() {
         element.style.display = 'grid';
     }
 }
+
+//Función para mostrar el valor del slider.
+
+const sliderValue = document.getElementById("sliderValue");
+const inputSlider = document.getElementById("cuantity");
+
+inputSlider.oninput = (() => {
+    let value = inputSlider.value;
+    sliderValue.textContent = value;
+});
+
+//Función para el contador de dinero:
+
+const display = document.getElementById("money");
+const img = document.getElementById("cerdito");
+let counter = 0;
+
+function add() {
+    counter++;
+    display.textContent = counter + " €";
+
+    if (counter > 999){
+        display.style = "color: red;"
+        counter = 999;
+    } 
+}
+
+
